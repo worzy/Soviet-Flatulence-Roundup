fabab896afa6047d1177b20f01e7fab3402c88ed
Ciaran Wood <ciaran.wood@mindcandy.com>
2012-11-05 15:40:40 +0000

    added some helpful comments

--- js/game.js
+++ js/game.js
@@ -1,15 +1,24 @@
 (function() {
     
+    //base component for Actors - i.e. Players, Enemies, NPCs etc.
     Crafty.c('Actor', {
         init: function() {
+            //uses the AnimationLoader component to load the sprites and data.
+            //we are reusing the sprites and data from BrowserQuest: https://github.com/mozilla/BrowserQuest
             this.requires('AnimationLoader');
             
             this.bind('AnimsLoaded', function() {
+                //once the animations are loaded, bind our movement handler to the NewDirection event
                 this.bind('NewDirection', this.onNewDirection);
+                
+                //set the initial animation to an idle one
                 this.animate('idle_down', 30, -1);
             });
         },
         onNewDirection: function(direction) {
+            //Handler for the 'NewDirection' event fire by the Fourway component.
+            //Here, we record the direction and if it changes, we swap the playing animation
+            
             if (direction.x < 0) {
                 if (this.direction !== 'left') {
                     this.stop().animate("walk_right", 10, -1).flip();
@@ -34,6 +43,9 @@
                     this.direction = 'down';
                 }
             }
+            
+            //if direction.x and direction.y are undefined or 0, then change animation
+            //to an idle one, using the last direction we stored.
             if(!direction.x && !direction.y) {
                 this.stop();
                 if (this.direction === 'left' || this.direction === 'right') {
@@ -47,9 +59,11 @@
         }
     });
     
+    //Player component - because of the way that the BrowserQuest assets are rendered,
+    //we have to compose an armor and a weapon component together.
     Crafty.c('Player', {
         init: function() {
-            this.player = Crafty.e('Actor, Fourway')
+            this.armor = Crafty.e('Actor, Fourway, Armor')
                 .animationLoader('goldenarmor');
             
             this.weapon = Crafty.e('Actor, Fourway, Weapon')
@@ -77,17 +91,24 @@
     };
     
     Game.prototype.mainScene = function() {
+        //create a player...
         var player = Crafty.e('Player');
         
-        var boss = Crafty.e('Actor, Boss')
-                        //render at [20, 20] and have a width and height of 48px
+        //and an enemy
+        var boss = Crafty.e('Actor, Boss, Enemy') //here, Boss and Enemy are just tags - we can use them
+                                                  //when checking for collisions
+                        //render at [500, 500]
                         .attr({x: 500, y: 500})
+                        
+                        //this loads up the sprite data
                         .animationLoader('boss');
     };
     
     window.onload = function() {
         var game = new Game();
         game.initCrafty();
+        
+        //play the main scene
         Crafty.scene('main');
     };
     
